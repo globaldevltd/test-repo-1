1de0fa4788658bac66c6b17329b6abc0d75488d9
lukeda <lukeda@gmail.com>
2020-02-28 19:04:21 +1000

    ./push.sh

--- push.sh
+++ push.sh
@@ -1,3 +1,3 @@
-MSG=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1`
-echo $MSG >> strings.txt
-git commit -am $MSG && git push
+HASH=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1`
+echo $HASH >> strings.txt
+git commit -am $0 || $HASH && git push
